c59057c9d9a76f6ffbb568cefb5e5b95c4305c9b
josepedrolorenzini <joseplorenzini@gmail.com>
2025-02-04 19:22:23 +1100

    main branch

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([git c
+        User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@sydneymobtech.com.au',
             'password' => bcrypt('password'),
